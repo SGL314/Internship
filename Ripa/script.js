d45510d3f500eb5844ef1ddfd25b43e6ab275777
SGL314 <x>
2025-04-08 16:50:39 -0300

    ripa doing

---
+++ Ripa/script.js
@@ -0,0 +1,114 @@
+const verbetes = [
+    {
+      id: "1",
+      title: "ABUNDÂNCIA",
+      content: ` Mulher graciosa, que portando uma bela grinalda de vagos fios se cinja a fronte, & o vestimento de cor verde, decorado em ouro, com a mão direita portando uma trombeta com fartura de muitos e variados frutos, uvas, azeitonas, & outros; & com esquerdo braço aperte um faixo de espigas de grão, de milho, painço, legumes, & semelhantes, do qual se vejam muitas de ditas espigas colhidas caírem, & espalharem-se ainda por Terra.
+      Bela, & graciosa deve ser pintada a Abundância, quer como coisa boa, & desejada por todos, quer como feia, & abominável ao produzir escassez, que dela é contrária.
+      Tem a grinalda cheia de flores, porque são as flores de frutos que fazem d’a Abundância mensageira, & autora; podem ainda significar a alegria, & as delícias da vida nos campos.
+      A cor verde, & os frisos de ouro de sua veste, são cores próprias fazendo com que o bom e verdejante campo floresça em férteis produções, para depois se renovarem, a maturação das aveias, & dos frutos, que permeiam a Abundância.
+      A trombeta da fartura pela fábula da Cabra Amalteia, contada por Hermógenes no livro da Frigia se também como faz riferimento Natale Conte no sétimo livro das suas metodologias ao capítulo dois de Aqueloo, & por isso Ovídio retrata dito Aqueloo sob a figura do Touro, no livro nove das Transformações, e sinal manifestado da Abundância, ele o diz.
+      Naiade; hoc pomis, & floris odore repletum 
+      Sacrarunt, diuesq; meo bona copia corriu est.
+      E porque a Abundância se diz Quantidade, para mostrá-la, assim,* a representamos, que o braço esquerdo tenha, (assim) como o direito a sua fartura*/carga*, & em benefício*, sendo que parte daquelas espigas espalham sobre a Terra.
+  In prescipitam Abundantia figuram,
+      Dominicus Ancaianus.
+  Aspice terrarum flauentes undique campos
+      Multiplici complet messe benigna Ceres.
+  Pomorum uario curuantur pondere rami.
+      Et bromio uitis plena liquore rubet.
+  Cerneboum pecuduu*q; greges hinc laetus humor:
+      Hinc pingui sidant vinima uineta lacu.
+  Sylua fera nutrit, perducunt aquora pisces,
+      Aerijs campis lata uagatur auis.
+  Quid iam depofitas proprio mortalis in usus
+      Nec celum quicquam Nec tibi terra negat.
+  `,
+    images: ['abundancia.png']
+    },
+    {
+      id: "2",
+      title: "ADOÇÃO DE MEDALHAS",
+      content: ` Do Senhor Giovanni Zaratino Castellini.
+      Duas figuras em toga que se sobrepõem as mãos direitas, para a concórdia de duas famílias diversas convertidas em uma, passando, o filho adotivo para a família que adota. è medalha de prata de Adriano Imperador, adotado por Traiano com tais inscrições. IMP. CAES. TRAIAN. HADRIAN. OPT. P. F. AVG. GERM. DAC. PART. HIC. DIVI. TRAIAN. AVG. P. M. TR. P. COS. P. P. ADOPTIO.
+      A mesma inscrição se vê em uma outra medalha com uma figura em pé tendo as mãos alçadas, & com a palavra PIETAS por que adotar um filho é ato de piedade, reconhecido ainda nesta medalha de Adriano Imperador o benefício da sua adoção pela piedade de Traiano que o adotou. As acima ditas mãos sobrepostas são símbola da concórdia, & a concórdia, assim como inclusive é a piedada símbolo de Adoção, como se pode ver na medalha de Paolo Emílio Lépido adotado pelo pai de Marco Lépido Triunv*iro, em cuja deitada está uma cabeça da concórdia velada assim expressa por Fulio Orfini. Pro Adoptonis symbolo concordiam, & Pietatem in antiquis denarijs positas essa saepe animaduertimus Paulus autem Lepidus adoptatus à patre M. Lepidi Triunuiri fuit, & ex aemilio Paullo, Paullus Aemilius Lepidus dic*e*o*tus est.
+  `,
+    },
+    {
+      id: "3",
+      title: "Preguiça",
+      content: `Mulher velha, feia, que está por se sentar, com a mão direita tento uma corda, e com a esquerda um caramujo, ou mesmo uma tartaruga.
+      A corda denota, que a preguiça liga, & vence os homens, e os faz inaptos a operar.
+      E o caramujo, ou tartaruga, demonstra a propriedade dos preguiçosos, que são ociosos, e não fazem nada.
+  `
+    },
+    {
+      id: "4",
+      title: "AQUISIÇÃO RUIM",
+      content: `
+      Homem vestido em cores de folhas de árvore que estão por cair; estará dita figura em ato de caminhar, & um pedaço da veste esteja preso em um espinho, provocando um grande corte, o que nos mostra o desprazer, que o sente, e na direita mão terá um milhafre que grasna.
+      Vestido por essas cores, pois assim com caem facilmente as folhas da árvore, assim também o caem, e se decaem as coisas não bem conquistadas; o mesmo demonstra o espinho, pois quando o homem não pensa às coisas de má conquista, mais cedo ou mais tarde o recebe dano, e vergonha.
+      Tem na mão direita um milhafre, para representar aquilo que a tal propósito disse Alciado*, traduzido em nossa língua.
+      L’eddace Nibbio mentre
+      Rece soverchio cibo, che rapio
+      Con la Madre si duol del fatto rio:
+      Dicendo. Ahiche del ventre
+      M’escon* l’interiora, e in gran periglio
+      Mi sento, & Ella à lui,
+      Non ti doler ò figlio    
+      Che’l tuo non perdinò; ma quel d’altrui.
+  
+  
+      `,
+    },
+    {
+      id: "5",
+      title: "AGUDEZ DO INGENHO",
+      content: `A Esfinge (como narra Pierio Valeriano) no livro VI sob a ponta da lança de Pálas, se como se via naquela estátua de Mminerva, que Plínio disse ser antigamente endereçada à Atenas) nos pode significar a Agudez do engenho, pois não se é ao mundo coisa assim tão fácil, pelo contrário tanto escondida, que a Agudez do engenho humano não pode descobrir, nem divulgar, assim como dito tenham em outro lugar ela como figura do engenho, porém se porá a pintar por tal demonstração Minerva naquela forma, que se deseja representar, mas que ainda que sobre a lança se tenha uma Esfinge, como dissemos.`,
+    },
+    {
+      id: "6",
+      title: "ADOLESCÊNCIA",
+      content: `Um jovenzinho vestido pomposamente, com a mão direita se apoiará uma arpa de tocar, e na esquerda trará um espelho, na cabeça uma grinalda de flores, se porá em pé sobre um relógio de pó/ampulheta, onde se denota que seja decaída já a areia da infância, e na outra parte temos um pavão.`,
+    },
+  ];
+  
+  function addContent(object) {
+   if(object.images){ 
+    addImage(object.images[0])
+   } else{
+  
+    const imgWrapper = document.querySelector(".img")
+      imgWrapper.innerHTML = ''
+   } 
+    const p = document.querySelector(".content p");  
+    p.innerHTML = object.content;
+  }
+  
+  function addImage(imagePath){
+   const img = document.createElement('img')
+   img.src = `imgs/${imagePath}`
+   const imgWrapper = document.querySelector(".img")
+   imgWrapper.innerHTML = ''
+   imgWrapper.append(img)
+  }
+  
+  function cleanActive() {
+    const activeElement = document.querySelector(".active");
+    if (activeElement) activeElement.classList.remove("active");
+  }
+  
+  const indice = document.querySelector(".indice ul");
+  
+  verbetes.forEach((item) => {
+    const li = document.createElement("li");
+    li.id = item.id;
+    li.innerHTML = item.title;
+    li.addEventListener("click", (e) => {
+      cleanActive();
+      e.target.classList.toggle("active");
+      addContent(item);
+    });
+  
+    indice.appendChild(li);
+  });
+  
