8debcffd00ebad47829224a5867c727db60dfdd4
SGL314 <samugonlob12@gmail.com>
2025-04-15 16:43:55 -0300

    Putting things

--- Ripa/script.js
+++ Ripa/script.js
@@ -1,114 +1,215 @@
 const verbetes = [
-    {
-      id: "1",
-      title: "ABUNDÂNCIA",
-      content: ` Mulher graciosa, que portando uma bela grinalda de vagos fios se cinja a fronte, & o vestimento de cor verde, decorado em ouro, com a mão direita portando uma trombeta com fartura de muitos e variados frutos, uvas, azeitonas, & outros; & com esquerdo braço aperte um faixo de espigas de grão, de milho, painço, legumes, & semelhantes, do qual se vejam muitas de ditas espigas colhidas caírem, & espalharem-se ainda por Terra.
-      Bela, & graciosa deve ser pintada a Abundância, quer como coisa boa, & desejada por todos, quer como feia, & abominável ao produzir escassez, que dela é contrária.
-      Tem a grinalda cheia de flores, porque são as flores de frutos que fazem d’a Abundância mensageira, & autora; podem ainda significar a alegria, & as delícias da vida nos campos.
-      A cor verde, & os frisos de ouro de sua veste, são cores próprias fazendo com que o bom e verdejante campo floresça em férteis produções, para depois se renovarem, a maturação das aveias, & dos frutos, que permeiam a Abundância.
-      A trombeta da fartura pela fábula da Cabra Amalteia, contada por Hermógenes no livro da Frigia se também como faz riferimento Natale Conte no sétimo livro das suas metodologias ao capítulo dois de Aqueloo, & por isso Ovídio retrata dito Aqueloo sob a figura do Touro, no livro nove das Transformações, e sinal manifestado da Abundância, ele o diz.
-      Naiade; hoc pomis, & floris odore repletum 
-      Sacrarunt, diuesq; meo bona copia corriu est.
-      E porque a Abundância se diz Quantidade, para mostrá-la, assim,* a representamos, que o braço esquerdo tenha, (assim) como o direito a sua fartura*/carga*, & em benefício*, sendo que parte daquelas espigas espalham sobre a Terra.
-  In prescipitam Abundantia figuram,
-      Dominicus Ancaianus.
-  Aspice terrarum flauentes undique campos
-      Multiplici complet messe benigna Ceres.
-  Pomorum uario curuantur pondere rami.
-      Et bromio uitis plena liquore rubet.
-  Cerneboum pecuduu*q; greges hinc laetus humor:
-      Hinc pingui sidant vinima uineta lacu.
-  Sylua fera nutrit, perducunt aquora pisces,
-      Aerijs campis lata uagatur auis.
-  Quid iam depofitas proprio mortalis in usus
-      Nec celum quicquam Nec tibi terra negat.
-  `,
-    images: ['abundancia.png']
-    },
-    {
-      id: "2",
-      title: "ADOÇÃO DE MEDALHAS",
-      content: ` Do Senhor Giovanni Zaratino Castellini.
-      Duas figuras em toga que se sobrepõem as mãos direitas, para a concórdia de duas famílias diversas convertidas em uma, passando, o filho adotivo para a família que adota. è medalha de prata de Adriano Imperador, adotado por Traiano com tais inscrições. IMP. CAES. TRAIAN. HADRIAN. OPT. P. F. AVG. GERM. DAC. PART. HIC. DIVI. TRAIAN. AVG. P. M. TR. P. COS. P. P. ADOPTIO.
-      A mesma inscrição se vê em uma outra medalha com uma figura em pé tendo as mãos alçadas, & com a palavra PIETAS por que adotar um filho é ato de piedade, reconhecido ainda nesta medalha de Adriano Imperador o benefício da sua adoção pela piedade de Traiano que o adotou. As acima ditas mãos sobrepostas são símbola da concórdia, & a concórdia, assim como inclusive é a piedada símbolo de Adoção, como se pode ver na medalha de Paolo Emílio Lépido adotado pelo pai de Marco Lépido Triunv*iro, em cuja deitada está uma cabeça da concórdia velada assim expressa por Fulio Orfini. Pro Adoptonis symbolo concordiam, & Pietatem in antiquis denarijs positas essa saepe animaduertimus Paulus autem Lepidus adoptatus à patre M. Lepidi Triunuiri fuit, & ex aemilio Paullo, Paullus Aemilius Lepidus dic*e*o*tus est.
-  `,
-    },
-    {
-      id: "3",
-      title: "Preguiça",
-      content: `Mulher velha, feia, que está por se sentar, com a mão direita tento uma corda, e com a esquerda um caramujo, ou mesmo uma tartaruga.
-      A corda denota, que a preguiça liga, & vence os homens, e os faz inaptos a operar.
-      E o caramujo, ou tartaruga, demonstra a propriedade dos preguiçosos, que são ociosos, e não fazem nada.
-  `
-    },
-    {
-      id: "4",
-      title: "AQUISIÇÃO RUIM",
-      content: `
-      Homem vestido em cores de folhas de árvore que estão por cair; estará dita figura em ato de caminhar, & um pedaço da veste esteja preso em um espinho, provocando um grande corte, o que nos mostra o desprazer, que o sente, e na direita mão terá um milhafre que grasna.
-      Vestido por essas cores, pois assim com caem facilmente as folhas da árvore, assim também o caem, e se decaem as coisas não bem conquistadas; o mesmo demonstra o espinho, pois quando o homem não pensa às coisas de má conquista, mais cedo ou mais tarde o recebe dano, e vergonha.
-      Tem na mão direita um milhafre, para representar aquilo que a tal propósito disse Alciado*, traduzido em nossa língua.
-      L’eddace Nibbio mentre
-      Rece soverchio cibo, che rapio
-      Con la Madre si duol del fatto rio:
-      Dicendo. Ahiche del ventre
-      M’escon* l’interiora, e in gran periglio
-      Mi sento, & Ella à lui,
-      Non ti doler ò figlio    
-      Che’l tuo non perdinò; ma quel d’altrui.
-  
-  
-      `,
-    },
-    {
-      id: "5",
-      title: "AGUDEZ DO INGENHO",
-      content: `A Esfinge (como narra Pierio Valeriano) no livro VI sob a ponta da lança de Pálas, se como se via naquela estátua de Mminerva, que Plínio disse ser antigamente endereçada à Atenas) nos pode significar a Agudez do engenho, pois não se é ao mundo coisa assim tão fácil, pelo contrário tanto escondida, que a Agudez do engenho humano não pode descobrir, nem divulgar, assim como dito tenham em outro lugar ela como figura do engenho, porém se porá a pintar por tal demonstração Minerva naquela forma, que se deseja representar, mas que ainda que sobre a lança se tenha uma Esfinge, como dissemos.`,
-    },
-    {
-      id: "6",
-      title: "ADOLESCÊNCIA",
-      content: `Um jovenzinho vestido pomposamente, com a mão direita se apoiará uma arpa de tocar, e na esquerda trará um espelho, na cabeça uma grinalda de flores, se porá em pé sobre um relógio de pó/ampulheta, onde se denota que seja decaída já a areia da infância, e na outra parte temos um pavão.`,
-    },
-  ];
-  
-  function addContent(object) {
-   if(object.images){ 
-    addImage(object.images[0])
-   } else{
-  
-    const imgWrapper = document.querySelector(".img")
-      imgWrapper.innerHTML = ''
-   } 
-    const p = document.querySelector(".content p");  
-    p.innerHTML = object.content;
-  }
-  
-  function addImage(imagePath){
-   const img = document.createElement('img')
-   img.src = `imgs/${imagePath}`
-   const imgWrapper = document.querySelector(".img")
-   imgWrapper.innerHTML = ''
-   imgWrapper.append(img)
-  }
-  
-  function cleanActive() {
-    const activeElement = document.querySelector(".active");
-    if (activeElement) activeElement.classList.remove("active");
-  }
-  
-  const indice = document.querySelector(".indice ul");
-  
-  verbetes.forEach((item) => {
-    const li = document.createElement("li");
-    li.id = item.id;
-    li.innerHTML = item.title;
-    li.addEventListener("click", (e) => {
-      cleanActive();
-      e.target.classList.toggle("active");
-      addContent(item);
+  {
+    id: "1",
+    title: "ABUNDÂNCIA",
+    content: ` Mulher graciosa, que portando uma bela grinalda de vagos fios se cinja a fronte, & o vestimento de cor verde, decorado em ouro, com a mão direita portando uma trombeta com fartura de muitos e variados frutos, uvas, azeitonas, & outros; & com esquerdo braço aperte um faixo de espigas de grão, de milho, painço, legumes, & semelhantes, do qual se vejam muitas de ditas espigas colhidas caírem, & espalharem-se ainda por Terra.
+    Bela, & graciosa deve ser pintada a Abundância, quer como coisa boa, & desejada por todos, quer como feia, & abominável ao produzir escassez, que dela é contrária.
+    Tem a grinalda cheia de flores, porque são as flores de frutos que fazem d’a Abundância mensageira, & autora; podem ainda significar a alegria, & as delícias da vida nos campos.
+    A cor verde, & os frisos de ouro de sua veste, são cores próprias fazendo com que o bom e verdejante campo floresça em férteis produções, para depois se renovarem, a maturação das aveias, & dos frutos, que permeiam a Abundância.
+    A trombeta da fartura pela fábula da Cabra Amalteia, contada por Hermógenes no livro da Frigia se também como faz riferimento Natale Conte no sétimo livro das suas metodologias ao capítulo dois de Aqueloo, & por isso Ovídio retrata dito Aqueloo sob a figura do Touro, no livro nove das Transformações, e sinal manifestado da Abundância, ele o diz.
+    Naiade; hoc pomis, & floris odore repletum 
+    Sacrarunt, diuesq; meo bona copia corriu est.
+    E porque a Abundância se diz Quantidade, para mostrá-la, assim,* a representamos, que o braço esquerdo tenha, (assim) como o direito a sua fartura*/carga*, & em benefício*, sendo que parte daquelas espigas espalham sobre a Terra.
+In prescipitam Abundantia figuram,
+    Dominicus Ancaianus.
+Aspice terrarum flauentes undique campos
+    Multiplici complet messe benigna Ceres.
+Pomorum uario curuantur pondere rami.
+    Et bromio uitis plena liquore rubet.
+Cerneboum pecuduu*q; greges hinc laetus humor:
+    Hinc pingui sidant vinima uineta lacu.
+Sylua fera nutrit, perducunt aquora pisces,
+    Aerijs campis lata uagatur auis.
+Quid iam depofitas proprio mortalis in usus
+    Nec celum quicquam Nec tibi terra negat.
+`,
+  images: ['abundancia.png']
+  },
+  {
+    id: "2",
+    title: "ARITMÉTICA",
+    content: `Mulher jovem, que com bela disposição, tenha em ambas as mãos o quadrado geométrico em ato de pegar a altura de uma Torre.`,
+    images: ['aritmetica.png']
+  },
+  {
+    id: "3",
+    title: "PREGUIÇA",
+    content: `Mulher velha, feia, que está por se sentar, com a mão direita tento uma corda, e com a esquerda um caramujo, ou mesmo uma tartaruga.
+    A corda denota, que a preguiça liga, & vence os homens, e os faz inaptos a operar.
+    E o caramujo, ou tartaruga, demonstra a propriedade dos preguiçosos, que são ociosos, e não fazem nada.`,
+    images: ['preguica.png']
+  },
+  {
+    id: "4",
+    title: "AMBIÇÃO",
+    content: `Mulher jovem, vestida de verde; com hábito sucinto, e com os pés nus; tem aos úmeros asas, & com ambas as mãos mostra de meter confusamente na cabeça várias sortes de Coroas, & tem os olhos vendados.`,
+    images: ['ambicao.png']
+  },
+  {
+    id: "5",
+    title: "AMIZADE",
+    content: `Mulher vestida de branco, porém descuidamente/levemente, mostre quase o lado esquerdo nu, & nu também o peito, com a mão direita mostra o coração no qual se fará um lema com as letras em ouro assim, Longe, & prope: & ao estremo da veste se verá escrito, Mors, & vita, será descabelada, & à cabeça levará uma grinalda de buxo, & de flores de pomos graúdos emaranhados juntos, na fronte se verá escrito. Hyems, & Aesi*tas.
+   Será descalça, & com o braço esquerdo terá uma Árvore seca, a qual feita circundada por uma vinha verde.`,
+    images: ['amizade.png']
+  },
+  {
+    id: "6",
+    title: "ADOLESCÊNCIA",
+    content: `Um jovenzinho vestido pomposamente, com a mão direita se apoiará uma arpa de tocar, e na esquerda trará um espelho, na cabeça uma grinalda de flores, se porá em pé sobre um relógio de pó/ampulheta, onde se denota que seja decaída já a areia da infância, e na outra parte temos um pavão.`,
+    images: ['semImagem.png']
+  },
+  {
+    id: "7",
+    title: "ACADEMIA",
+    content: `Mulher vestida de multicores, de aspecto, & idade viril, coroada em ouro, na mão direita terá uma lima, e em torno de seu punho o escrito DETRAHIT ATQVE POLIT, na mão esquerda possuirá uma guirlanda trabalhada em Louro, Hera, Mirto, e na mesma mão, pendem um par de pomos granados, se senterá em uma cadeira enfeitada por verias folhas, e frutos de Cidra, Cipreste, e Carvalho, com ainda ramos de Oliveira, na parte onde se apóia o cotovelo, o mais próximo possível da figura. Estará no meio de um jardim sombrio, lugar “cheio de natureza/matagal”: com Plátanos em torno dos pés, portará grande quantidade de livros, dentre os quais se faz presente um Cinocéfalo, ou mesmo Babuíno.
+`, images: ['academia.png']
+  },
+  {
+    id: "8",
+    title: "ADULAÇÃO",
+    content: `Uma mulher vestida de hábito artificial, & vago, que toca a tíbia, ou a flauta, e um cervo, que o esteja adormecendo próximo aos pés: assim a pinta Oro Apolline, e Pierio Valeriano no livro VII dos seus Hieroglifos, e citam alguns, que o cervo por sua natureza tentado pelo som da flauta, quase se esquece de si mesmo, e se deixa pegar. Em conformidade do que se está na presente imagem, na qual se declara a doçura das palavras com a melodia do som, e a natureza de quem voluntariamente se sente adular com o infeliz natural instinto do Cervo o qual mostra ainda, que é tímido, e de ânimo débil, quem voluntariamente se põe aos ouvidos dos aduladores.`,
+    images: ['adulacao.png']
+  },
+  {
+    id: "9",
+    title: "AJUDA",
+    content: `Homem de idade viril, vestido de cor branca, & sobre o dito vestimento estará um manto em cor púrpura, & pelo Céu se vê um claríssimo raio que ilumina dita figura, será coroada com uma guirlanda de Oliveira, terá ao pescoço uma Corrente de ouro & por pendente um coração, estará com o braço direito estendido, & com a mão aberta, & com a esquerda tenha um cajado fincado em terra circundado de uma verdejante, & frutífera vinha, & da parte direita estará uma Cegonha.`,
+    images: ['ajuda.png']
+  },
+  {
+    id: "10",
+    title: "ALEGRIA",
+    content: ` Jovenzinha com fronte carnuda, lisa, e grande, será vestida em branco, e dito vestimento pintado em verdes frutos, e flores vermelhas, e amarelas, com uma guirlanda de várias flores na cabeça, na mão direita tenha um jarro de cristal cheio de vinho vermelho e saudável, e na esquerda uma grande taça de ouro, esteja em aspecto gracioso, e belo, e prontamente se mostra a dançar em um prado cheio de flores.`,
+    images: ['alegria.png']
+  },
+  {
+    id: "11",
+    title: "ALTERIDADE DA PESSOA CIVIL NASCIDA POBRE",
+    content: `Mulher jovem, cega, com a face austera, será vestida em uma rica, & pomposa blusa de cor vermelha, toda contexta de diversas jóias de grande valor, & sobre a dita blusa terá uma veste de altíssimo/vilíssimo valor toda remendada em cores da terra, ou mesmo da cinza, terá debaixo do braço direito um Pavão, e o esquerdo, com a mão aberta, estará com um pé sobre uma grande bola, & o outro em ato de precipitar-se de dita bola.`,
+    images: ['alteridade_pcnp.png']
+  },
+  {
+    id: "12",
+    title: "MAESTRIA",  
+    content: `Homem de aspecto magnífico, & venerável, com veste longa, & cheio de magnânima gravidade/pompa, com um espelho na mão, em volta do qual será um lema com estas palavras. INSPICE, CAVTVS ERIS.`,
+    images: ['maestria.png']
+  },
+  {
+    id: "",
+    title: "",
+    content: ``,
+    images: ['semImagem.png']
+  },
+  {
+    id: "",
+    title: "",
+    content: ``,
+    images: ['semImagem.png']
+  },
+  {
+    id: "",
+    title: "",
+    content: ``,
+    images: ['semImagem.png']
+  },
+  {
+    id: "",
+    title: "",
+    content: ``,
+    images: ['semImagem.png']
+  },
+];
+
+function addContent(object) {
+  if(object.images){ 
+  addImage(object.images[0])
+  } else{
+
+  const imgWrapper = document.querySelector(".img")
+    imgWrapper.innerHTML = ''
+  } 
+  const p = document.querySelector(".content p");  
+  p.innerHTML = "<br>"+object.content; // para não ficar colado com a imagem
+}
+
+function addImage(imagePath){ // called by addContent(arg)
+  const img = document.createElement('img')
+  img.src = `imgs/${imagePath}`
+  img.id = "imageBigZoom"
+  if (imagePath != "semImagem.png"){
+    img.className = "imageWithZoom";
+
+    img.addEventListener("mouseenter", () => { // GPT: código não roda
+      const prop = screen.height/img.height; // GPT: como pegar o tamanho da tela
+      
+      img.style = "position: relative;height: 100vh;width: 100%;object-fit: cover;"; // GPT: para naõ distorcer imagem "object-fit: cover;"
     });
+
+    img.addEventListener("mouseleave", () => {
+      img.style = "width: auto; height: auto; transition: transform 0.5s ease;";
+    });
+  }
+
   
-    indice.appendChild(li);
+
+  const imgWrapper = document.querySelector(".img")
+  imgWrapper.innerHTML = ''
+  imgWrapper.append(img)
+  }
+
+function cleanActive() {
+  const activeElement = document.querySelector(".active");
+  if (activeElement) activeElement.classList.remove("active");
+}
+
+const indice = document.querySelector(".indice ul");
+
+// Move o menu quando chega perto
+
+function dinamic(){
+  const menu = null;
+  menu.addEventListener("mouseenter", () => {
   });
-  
+}
+dinamic();
+
+// Ordena os itens
+
+var verbetesOrdenados = [];
+var verbs = [];  
+
+verbetes.forEach((verb) => {  
+  verbs.push(verb);
+});
+
+verbs.forEach((item) => console.log(item.title));
+while (verbs.length > 0){
+  var sel = null;
+  var i = 0, index = 0;
+  verbs.forEach((verb) => {
+    if (sel == null) sel = verb;
+    else if (sel.title > verb.title){
+      sel = verb;
+      index = i;
+    }
+    i++;
+  })
+  verbetesOrdenados.push(sel);
+  verbs = verbs.filter(item => item.title !== sel.title);
+
+}
+console.log("ola");
+
+verbetesOrdenados.forEach((item) => console.log(item.title));
+verbetesOrdenados.forEach((item) => {
+  const li = document.createElement("li");
+  li.id = item.id
+  li.innerHTML = item.title;
+  li.addEventListener("click", (e) => {
+    cleanActive();
+    e.target.classList.toggle("active");
+    addContent(item);
+  });
+
+  indice.appendChild(li);
+});
