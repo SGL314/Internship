550fa57bd007b7a3346527d007e741f66c62bfb5
Your Name~ <you@example.com>
2025-05-20 15:11:24 -0300

    vários _gramas

---
+++ StopWords/Trigramas/pdfToTxt.py
@@ -0,0 +1,35 @@
+# Transforma .pdf em .txt
+import PyPDF2
+file = "aRevolucaoDosBichos"
+PATH = "content/"
+reader = PyPDF2.PdfReader(""+PATH+""+file+".pdf")
+qt = 0
+last = 0
+
+with open(PATH+"txt_cap1.txt",'w',encoding="utf-8") as txt_file:
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        text = page.extract_text()
+        # verifica passagem de capítulo
+        block = ["1","1","1","1"]
+        result = ""
+        ardb = open(PATH+"ardb.txt","a",encoding="utf-8")
+        # ardb.write(text) # +"\n\n\n\n\n ------------------------------------------------------------------------ \n\n\n\n\n"
+        for char in text:
+            block[0] = block[1]
+            block[1] = block[2]
+            block[2] = block[3]
+            block[3] = char
+            # qt+=1
+            # if (qt >= 14491) :
+            # ardb.write(f"{block}\n")
+            for num in range(1,11):
+                if ((block[1] == f"{num}" and block[2] == "." and block[3] == "\n") or (block[1] == "1" and block[2] == "0" and block[3] == "." and num==10)):
+                    if last>num:
+                        break
+                        pass
+                    print(f"cap {num}: {block} : {last}")
+                    last = num
+                    txt_file = open(PATH+f"txt_cap{num}.txt","w",encoding="utf-8")
+        txt_file.write(text)
+#
