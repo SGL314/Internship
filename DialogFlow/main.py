7963bbcf92e9d13857373f24cb62be1f1f6636c2
SGL314 <samugonlon12@gmail.com>
2025-06-30 23:38:55 -0300

    sync bro

---
+++ DialogFlow/main.py
@@ -0,0 +1,21 @@
+__author__='thiagocastroferreira'
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+pedidos = []
+
+@app.route('/', methods=['POST'])
+def main():
+    data = request.get_json(silent=True)
+
+    print(data)
+    
+    return jsonify(data)
+
+# run Flask app
+if __name__ == "__main__":
+    app.debug = False
+    app.run()
